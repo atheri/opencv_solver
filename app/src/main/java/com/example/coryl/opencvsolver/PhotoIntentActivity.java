79f3a71cff3a6ab2d1768f71a1cdcc53d0f24ce6
atheri <corylotze@gmail.com>
2016-04-19 03:05:08 -0700

    Reworked picture taking and previewing in the app.
    Changed to a more standard procedure for resizing the image from the
    camera to be displayed. Added a directory for the app to store pictures.

--- app/src/main/java/com/example/coryl/opencvsolver/PhotoIntentActivity.java
+++ app/src/main/java/com/example/coryl/opencvsolver/PhotoIntentActivity.java
@@ -2,12 +2,12 @@ package com.example.coryl.opencvsolver;
 
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.Environment;
 import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.DisplayMetrics;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -15,94 +15,136 @@ import android.widget.ImageView;
 import android.widget.Toast;
 
 import java.io.File;
-import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.Locale;
 
 public class PhotoIntentActivity extends AppCompatActivity {
 
-    private static final int CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE = 100;
+    // Activity request codes
+    private static final int CAMERA_CAPTURE_IMAGE_REQUEST_CODE = 100;
+    //private static final int CAMERA_CAPTURE_VIDEO_REQUEST_CODE = 200;
+    public static final int MEDIA_TYPE_IMAGE = 1;
+    public static final int MEDIA_TYPE_VIDEO = 2;
+
+    // directory name to store captured image
+    private static final String IMAGE_DIRECTORY_NAME = "OpenCV Solver";
+
+    // file url to store image
     private Uri fileUri;
-    private ImageView mImageView;
+
+
+    private ImageView imgPreview;
+    private Button btnCapturePicture;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
 
-        this.mImageView = (ImageView) this.findViewById(R.id.imageView1);
-        Button photoButton = (Button) this.findViewById(R.id.button1);
-        assert photoButton != null;
-        photoButton.setOnClickListener(new View.OnClickListener() {
+        imgPreview = (ImageView) findViewById(R.id.imgPreview);
+        btnCapturePicture = (Button) findViewById(R.id.btnCapturePicture);
+
+        // Button capture image listener
+        btnCapturePicture.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-                fileUri = getOutputMediaFileUri();
-                cameraIntent.putExtra(MediaStore.EXTRA_OUTPUT, fileUri);
-                startActivityForResult(cameraIntent, CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE);
+                captureImage();
             }
         });
     }
 
     @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        if (requestCode == CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE ) {
-            if (resultCode == RESULT_OK) {
-                Toast.makeText(this, "is null", Toast.LENGTH_LONG).show();
-                DisplayMetrics metrics = new DisplayMetrics();
-                getWindowManager().getDefaultDisplay().getMetrics(metrics);
-                try {
-                    Bitmap orig = MediaStore.Images.Media.getBitmap(getContentResolver(), fileUri);
-                    int width = orig.getWidth();
-                    int height = orig.getHeight();
-                    double scale = 1;
-                    if (width > 4096 || height > 4096) {
-                        if (width > height) {
-                            scale = 4096/(double)width;
-                        } else {
-                            scale = 4096/(double)height;
-                        }
-                    }
-                    width = (int)(width*scale);
-                    height = (int)(height*scale);
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
 
-                    Bitmap photo = Bitmap.createScaledBitmap(orig, width, height, true);
-                    mImageView.setImageBitmap(photo);
-                } catch (IOException e) {
-                    Log.e(getString(R.string.app_name), "exception", e);
-                }
+        // save file url in bundle as it will be null on screen orientation changes
+        outState.putParcelable("file_uri", fileUri);
+    }
+
+    @Override
+    protected void onRestoreInstanceState(Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+
+        // restore file url
+        fileUri = savedInstanceState.getParcelable("file_uri");
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        // if result is image capture
+        if (requestCode == CAMERA_CAPTURE_IMAGE_REQUEST_CODE) {
+            if (resultCode == RESULT_OK) {
+                // Capture success
+                // display in preview
+                previewCapturedImage();
             } else if (resultCode == RESULT_CANCELED) {
-                //TODO//user cancelled the image capture
+                // user cancelled image capture
+                Toast.makeText(getApplicationContext(), "User cancelled image capture", Toast.LENGTH_SHORT).show();
             } else {
-                //TODO//image capture failed, do something
+                // failed to capture image
+                Toast.makeText(getApplicationContext(), "Failed to capture image", Toast.LENGTH_SHORT).show();
             }
         }
     }
 
-    /** Create a file Uri for saving an image or video */
-    private Uri getOutputMediaFileUri(){
-        return Uri.fromFile(getOutputMediaFile());
+    // Launches camera app via intent
+    private void captureImage() {
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+
+        fileUri = getOutputMediaFileUri(MEDIA_TYPE_IMAGE);
+
+        intent.putExtra(MediaStore.EXTRA_OUTPUT, fileUri);
+
+        // start image capture intent
+        startActivityForResult(intent, CAMERA_CAPTURE_IMAGE_REQUEST_CODE);
     }
 
-    /** Create a File for saving an image or video */
-    private File getOutputMediaFile() {
-        // Create an image file name
-        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss", Locale.US).format(new Date());
-        String imageFileName = "JPEG_" + timeStamp + "_";
-        File storageDir = Environment.getExternalStoragePublicDirectory(
-                Environment.DIRECTORY_PICTURES);
-
-        File image = null;
+    private void previewCapturedImage() {
         try {
-            image = File.createTempFile(
-                    imageFileName,  /* prefix */
-                    ".jpg",         /* suffix */
-                    storageDir      /* directory */
-            );
-        } catch (IOException e) {
-            Log.e(getString(R.string.app_name), "exception", e);
+            BitmapFactory.Options options = new BitmapFactory.Options();
+
+            options.inSampleSize = 4;
+
+            final Bitmap bitmap = BitmapFactory.decodeFile(fileUri.getPath(), options);
+            imgPreview.setImageBitmap(bitmap);
+        } catch (NullPointerException e) {
+            e.printStackTrace();
         }
-        return image;
+    }
+
+    // Creating file uri to store image/video
+    public Uri getOutputMediaFileUri(int type) {
+        return Uri.fromFile(getOutputMediaFile(type));
+    }
+
+    // returning image / video
+    private static File getOutputMediaFile(int type) {
+
+        // External sdcard location
+        File mediaStorageDir = new File(
+                Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES), IMAGE_DIRECTORY_NAME);
+
+        // Create the storage directory if it does not exist
+        if (!mediaStorageDir.exists()) {
+            if (!mediaStorageDir.mkdirs()) {
+                Log.d(IMAGE_DIRECTORY_NAME, "Oops! Failed create " + IMAGE_DIRECTORY_NAME + " directory");
+                return null;
+            }
+        }
+
+        // Create a media file name
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss",
+                Locale.getDefault()).format(new Date());
+        File mediaFile;
+        if (type == MEDIA_TYPE_IMAGE) {
+            mediaFile = new File(mediaStorageDir.getPath() + File.separator + "IMG_" + timeStamp + ".jpg");
+        } else if (type == MEDIA_TYPE_VIDEO) {
+            mediaFile = new File(mediaStorageDir.getPath() + File.separator + "VID_" + timeStamp + ".mp4");
+        } else {
+            return null;
+        }
+
+        return mediaFile;
     }
 }
